118e9bff79f98ebb555fcb52d79d0f8605fe2f57
LingIgor <lingigor1990@gmail.com>
2023-05-24 14:06:49 +0300

    second

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,6 @@
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+  return ( fetch ('https://api.themoviedb.org/3/movie/550?api_key=3f267efdd2dfa6b504977d892a47bb84').then( res => res.json()).then(data => console.log( data ))
+   
+   
   );
 };
