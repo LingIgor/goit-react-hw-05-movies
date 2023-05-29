b0102676d8e08f9b5b1e33b4f1b9928472260b4b
Ling Igor <lingigor1990@gmail.com>
2023-05-29 23:05:06 +0300

    last commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename={'/goit-react-hw-05-movies'}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
